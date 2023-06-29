a6c5b26a56bc8a981c3708fc20487f4cff79b5d2
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-29 19:07:04 +0100

    added files

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,14 +18,40 @@ const Home = () => {
   return (
     <div className="container">
       <div className="header">
+        <div className="top-bar">
+          <div className="logo-and-search">
+            <img
+              className="github-logo"
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub Logo"
+            />
+            <input className="search-bar" type="text" placeholder="Search" />
+          </div>
+          <div className="navigation">
+            <Link to="/">Pull requests</Link>
+            <Link to="/">Issues</Link>
+            <Link to="/">Codespaces</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Explore</Link>
+          </div>
+          <div className="profile">
+            <img
+              className="profile-logo"
+              src="https://avatars.githubusercontent.com/u/1?s=60&v=4"
+              alt="Profile"
+            />
+            <button className="add-button">+</button>
+          </div>
+        </div>
         <h1>GitHub Jobs</h1>
         <p>Find your next opportunity at GitHub</p>
       </div>
       <ul className="jobs-list">
         {jobs.map(job => (
           <li key={job.id} className="jobs-list-item">
-            <h2>{job.title}</h2>
-            <p className="job-description">{job.description}</p>
+            <h2 className="job-title">{job.title}</h2>
+            <p className="job-location">{job.location}</p>
+            <p className="job-description">{job.description.substring(0, 200)}...</p>
             <Link className="link-button" to={`/job/${job.id}`}>
               View Details
             </Link>
